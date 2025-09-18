63b97ace19f9dc8c44b07883549cd344b8b4ecf5
Juk-kruw <sawana.thi@student.mahidol.edu>
2025-09-18 11:52:15 +0700

    Update booking.java
    add print booking

--- booking.java
+++ booking.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
  *
  * @author sawan
  */
-public class booking  {
+public class booking {
 
     String bookingID;
     String customerID;
@@ -15,16 +15,18 @@ public class booking  {
     int[] RoomsPerDay;
     int Persons;
     int[] MealsPerPersonPerDay;
-    int [] priceRoom = {8000,11000,18000};
-    int [] priceDish = {100,450,600};
+    int[] priceRoom = {8000, 11000, 18000};
+    int[] priceDish = {100, 450, 600};
     int RoomPricePerday;
     int DishPricePerday;
     int total;
     int finalprice = 0;
+
     public void setFinalPrice(int discountpercent) {
-       this.finalprice = total-(total*discountpercent/100);
-    
+        this.finalprice = total - (total * discountpercent / 100);
+
     }
+
     private int[] parseTriple(String s) {
         String[] in = s.split(":");
         return new int[]{
@@ -43,9 +45,23 @@ public class booking  {
         this.RoomsPerDay = parseTriple(parts[3]);
         this.Persons = Integer.parseInt(parts[4]);
         this.MealsPerPersonPerDay = parseTriple(parts[5]);
-        RoomPricePerday = RoomsPerDay[0]*priceRoom[0]+RoomsPerDay[1]*priceRoom[1]+RoomsPerDay[2]*priceRoom[2];
-        DishPricePerday = MealsPerPersonPerDay[0]*priceDish[0]+MealsPerPersonPerDay[1]*priceDish[1]+MealsPerPersonPerDay[2]*priceDish[2];
-        total = RoomPricePerday*day+DishPricePerday*day;
+
+        this.RoomPricePerday = RoomsPerDay[0] * priceRoom[0]
+                + RoomsPerDay[1] * priceRoom[1]
+                + RoomsPerDay[2] * priceRoom[2];
+
+        this.DishPricePerday = MealsPerPersonPerDay[0] * priceDish[0]
+                + MealsPerPersonPerDay[1] * priceDish[1]
+                + MealsPerPersonPerDay[2] * priceDish[2];
+        this.total = RoomPricePerday * day + DishPricePerday * day;
+    }
+
+    public void print_booking() {
+
+        System.out.printf("Booking %S, customer %S  >>  days = %d, persons = %d,  rooms=[%d,%d,%d],  meals = [%d,%d,%d] \n",
+                bookingID, customerID, day, Persons, RoomsPerDay[0], RoomsPerDay[1],
+                RoomsPerDay[2], MealsPerPersonPerDay[0], MealsPerPersonPerDay[1], MealsPerPersonPerDay[2]);
+
     }
 
   
