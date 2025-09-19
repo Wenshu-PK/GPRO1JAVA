5d3b3a156e644ca46ac1435457f521614e7ed86d
Juk-kruw <sawana.thi@student.mahidol.edu>
2025-09-19 11:00:28 +0700

    Update main.java
    add Array List for booking class and load booking from file method

--- main.java
+++ main.java
@@ -1,1 +1,30 @@
+package Project1_6713249;
 
+import java.util.*;
+
+public class GPRO1JAVA {
+
+
+   
+  protected ArrayList<booking> booking_list = new ArrayList<>();
+
+    
+  public void load_booking(String file_path) 
+  {
+        Scanner readFile = new Scanner(file_path);
+        String line;
+        readFile.nextLine();
+        while (readFile.hasNext()) 
+        {
+
+            line = readFile.nextLine();
+            booking_list.add(new booking(line));
+
+        }
+
+    }
+
+
+
+  
+}
