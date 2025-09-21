66e0350552a365e443e83e464d6d5ca6be3d9664
Juk-kruw <sawana.thi@student.mahidol.edu>
2025-09-22 01:38:56 +0700

    add exception handle

--- booking.java
+++ booking.java
@@ -12,16 +12,18 @@ public class booking {
     private String bookingID;
     private String customerID;
     private int day;
-    private int[] RoomsPerDay;
+    private int[] RoomsPerDay;//index 1 for R1,index 2 for R2,index 3 for R3
     private int Persons;
-    private int[] MealsPerPersonPerDay;
+    private int[] MealsPerPersonPerDay;//index 1 for M1,index 2 forM2,index 3 for M3
     private int RoomPricePerday;
     private int DishPricePerday;
     private int total;
     private int finalprice = 0;
 
-    private static int[] parseTriple(String s) {
+    private static int[] parseTriple(String s) throws InvalidNumberException{
         String[] in = s.split(":");
+       if(in.length!=3)
+           throw new InvalidNumberException(s);
         return new int[]{
             Integer.parseInt(in[0]),
             Integer.parseInt(in[1]),
@@ -36,66 +38,71 @@ public class booking {
         }
 
     }
-    
 
-    public void setFinalPrice(int discountpercent) {
-        this.finalprice = total - (total * discountpercent / 100);
-
-    }
-  
     public static booking loadbooking(String line) throws InvalidNumberException, NumberFormatException {
 
         String[] parts = line.split(",");
-       
 
         String bookID = parts[0].trim();
         String cusID = parts[1].trim();
+
+        // Parse days
         int days;
         try {
             days = Integer.parseInt(parts[2].trim());
         } catch (NumberFormatException e) {
-            throw new NumberFormatException("NumberFormatException For day : " + parts[2].trim() + "\n" + line);
-        }if (Integer.parseInt(parts[2].trim()) < 1) {
-            throw new InvalidNumberException("InvalidNumberException For day : " + parts[2].trim() + "\n" + line);
+            throw new NumberFormatException("NumberFormatException for days: " + parts[2].trim() + "\n" + line);
         }
-       
-        days = Integer.parseInt(parts[2].trim());
+        if (days < 1) {
+            throw new InvalidNumberException("InvalidNumberException for days : " + days + "\n" + line);
+        }
 
+        // Parse rooms
         int[] RoomsDay;
-        if (parseTriple(parts[3].trim()).length != 3) {
-            throw new InvalidNumberException("InvalidNumberException For room :" + parts[3].trim() + "\n" + line);
-        }try {
+
+        try {
             RoomsDay = parseTriple(parts[3].trim());
+            
+            for (int r : RoomsDay) {
+                if (r < 0) {
+                    throw new InvalidNumberException("InvalidNumberException for room : " + r + "\n" + line);
+                }
+            }
+
         } catch (NumberFormatException e) {
-            throw new NumberFormatException("NumberFormatException For room :" + parts[3].trim() + "\n" + line);
-        }
-        
-        RoomsDay = parseTriple(parts[3].trim());
+            throw new NumberFormatException("NumberFormatException for room: " + parts[3].trim() + "\n" + line);
+        } catch(InvalidNumberException e){
+        throw new InvalidNumberException("InvalidNumberException for room : " + parts[3].trim() + "\n" + line);}
+
+        // Parse person
         int Person;
         try {
             Person = Integer.parseInt(parts[4].trim());
-        } catch (NumberFormatException e) {
-            throw new NumberFormatException("NumberFormatException For Person : " + parts[4].trim() + "\n" + line);
-        }
-        if (Integer.parseInt(parts[4].trim()) < 1) {
-            throw new InvalidNumberException("InvalidNumberException For Person : " + parts[4].trim() + "\n" + line);
-        }
-        
-        Person = Integer.parseInt(parts[4].trim());
+            if (Person < 1) {
+                throw new InvalidNumberException("InvalidNumberException for person : " + Person + "\n" + line);
+            }
 
-        int[] MealsDay;
-        if (parseTriple(parts[5].trim()).length != 3) {
-            throw new InvalidNumberException("InvalidNumberException For meal : " + parts[5].trim() + "\n" + line);
+        } catch (NumberFormatException e) {
+            throw new NumberFormatException("NumberFormatException for person: " + parts[4].trim() + "\n" + line);
         }
+
+        // Parse meals
+        int[] MealsDay;
         try {
             MealsDay = parseTriple(parts[5].trim());
+            
+            for (int m : MealsDay) {
+                if (m < 0) {
+                    throw new InvalidNumberException("InvalidNumberException for meal : " + m + "\n" + line);
+                }
+            }
         } catch (NumberFormatException e) {
-            throw new NumberFormatException("NumberFormatException For meal : " + parts[5].trim() + "\n" + line);
-        }
-        MealsDay = parseTriple(parts[5].trim());
+            throw new NumberFormatException("NumberFormatException for meal: " + parts[5].trim() + "\n" + line);
+        }catch(InvalidNumberException e){
+        throw new InvalidNumberException("InvalidNumberException for meal : " + parts[5].trim() + "\n" + line);}
 
+        // Return new booking object
         return new booking(bookID, cusID, days, RoomsDay, Person, MealsDay);
-
     }
 
     public booking(String BookingID,
@@ -111,7 +118,7 @@ public class booking {
         this.RoomsPerDay = RoomPerDays;
         this.Persons = PerSons;
         this.MealsPerPersonPerDay = MealsPerPersonPerDays;
-        
+
     }
 
     public void print_booking() {
@@ -121,9 +128,17 @@ public class booking {
                 RoomsPerDay[2], MealsPerPersonPerDay[0], MealsPerPersonPerDay[1], MealsPerPersonPerDay[2]);
 
     }
-    
-    public double getSubTotal(){return this.total;}
-    public String getBookingId(){return this.bookingID;}
-    public String getCustomerID(){return this.customerID;}
+
+    public double getSubTotal() {
+        return this.total;
+    }
+
+    public String getBookingId() {
+        return this.bookingID;
+    }
+
+    public String getCustomerID() {
+        return this.customerID;
+    }
 
 }
